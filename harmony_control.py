10b65f3588378421dfaa8e319f9c935a45fc6830
Cyrus <cyrushadavi>
2016-04-21 20:16:34 -0700

    got rid of unnecessary thing

--- harmony_control.py
+++ harmony_control.py
@@ -7,7 +7,7 @@ import argparse
 config_dict = config.get_dict_of_params()
 args = argparse.Namespace()
 
-def watch_tv(parser):
+def watch_tv():
     args.activity='watch tv'
     args.email=config_dict['harmony_email']
     args.harmony_ip=config_dict['harmony_ip']
@@ -15,7 +15,7 @@ def watch_tv(parser):
     args.password=config_dict['harmony_password']
     harmony.start_activity(args)
 
-def watch_fire_tv(parser):
+def watch_fire_tv():
     args.activity='watch fire tv'
     args.email=config_dict['harmony_email']
     args.harmony_ip=config_dict['harmony_ip']
